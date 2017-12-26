6ade13e7351ff4828e3771509dc40c187ee500e8
Hodogor <kop.lop2012@yandex.ru>
2017-12-26 09:08:15 +0300

    fsd
    fsdf

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app 
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
