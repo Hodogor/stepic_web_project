989637f4d35f156f7811f3f92d28821b4243d0c0
Hodogor <kop.lop2012@yandex.ru>
2017-12-26 08:27:06 +0300

    new

--- hello.py
+++ hello.py
@@ -1,11 +1,4 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web/ask',
-    'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8000',
-        '--workers=16',
-        '--timeout=60',
-        'ask.wsgi:application',
-    ),
-}
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
